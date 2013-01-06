f4ea4108c866b565401ec389d5bfb6b48a8935f3
Aleksey Demakov <ademakov@gmail.com>
2013-01-06 23:40:27 +0300

    Add logging to runq.

--- src/runq.c
+++ src/runq.c
@@ -25,52 +25,66 @@
 void
 mm_runq_init(struct mm_runq *runq)
 {
+	ENTER();
 	ASSERT(MM_RUNQ_BINS <= (8 * sizeof(runq->bmap)));
 
 	runq->bmap = 0;
 	for (int i = 0; i < MM_RUNQ_BINS; i++) {
 		mm_list_init(&runq->bins[i]);
 	}
+
+	LEAVE();
 }
 
 struct mm_task *
 mm_runq_get_task(struct mm_runq *runq)
 {
-	if (unlikely(!runq->bmap))
-		return NULL;
+	ENTER();
 
-	int priority = ctz(runq->bmap);
-	ASSERT(priority >= 0 && priority < MM_RUNQ_BINS);
-	ASSERT(!mm_list_empty(&runq->bins[priority]));
+	struct mm_task *task = NULL;
+	if (likely(runq->bmap)) {
+		int priority = ctz(runq->bmap);
+		ASSERT(priority >= 0 && priority < MM_RUNQ_BINS);
+		ASSERT(!mm_list_empty(&runq->bins[priority]));
 
-	struct mm_list *head = mm_list_head(&runq->bins[priority]);
-	struct mm_task *task = containerof(head, struct mm_task, queue);
-	ASSERT(priority == task->priority);
+		struct mm_list *link = mm_list_head(&runq->bins[priority]);
+		task = containerof(link, struct mm_task, queue);
+		ASSERT(priority == task->priority);
 
-	mm_list_delete(head);
-	if (mm_list_empty(&runq->bins[priority]))
-		runq->bmap &= ~(1 << priority);
+		mm_list_delete(link);
+		if (mm_list_empty(&runq->bins[priority]))
+			runq->bmap &= ~(1 << priority);
+	}
 
+	LEAVE();
 	return task;
 }
 
 void
 mm_runq_put_task(struct mm_runq *runq, struct mm_task *task)
 {
+	ENTER();
+
 	int priority = task->priority;
 	ASSERT(priority >= 0 && priority < MM_RUNQ_BINS);
 
 	runq->bmap |= (1 << priority);
 	mm_list_append(&runq->bins[priority], &task->queue);
+
+	LEAVE();
 }
 
 void
 mm_runq_delete_task(struct mm_runq *runq, struct mm_task *task)
 {
+	ENTER();
+
 	int priority = task->priority;
 	ASSERT(priority >= 0 && priority < MM_RUNQ_BINS);
 
 	mm_list_delete(&task->queue);
 	if (mm_list_empty(&runq->bins[priority]))
 		runq->bmap &= ~(1 << priority);
+
+	LEAVE();
 }
