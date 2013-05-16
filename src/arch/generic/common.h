e504576b45063b7876259c230b54308aaf41c385
Aleksey Demakov <ademakov@gmail.com>
2013-05-17 03:16:07 +0400

    Add arch-specific common headers.

---
+++ src/arch/generic/common.h
@@ -0,0 +1,33 @@
+/*
+ * arch/generic/common.h - MainMemory arch-specific basic properties.
+ *
+ * Copyright (C) 2013  Aleksey Demakov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ARCH_GENERIC_COMMON_H
+#define ARCH_GENERIC_COMMON_H
+
+/* General purpose register width. */
+#define MM_WORD_32BIT		1
+/*define MM_WORD_64BIT		0*/
+
+/* Cache line size. */
+#define MM_CACHELINE		(64)
+
+/* Virtual memory page size. */
+#define MM_PAGE_SIZE		(4096)
+
+#endif /* ARCH_GENERIC_COMMON_H */
---
+++ src/arch/x86-64/common.h
@@ -0,0 +1,33 @@
+/*
+ * arch/x86-64/common.h - MainMemory arch-specific basic properties.
+ *
+ * Copyright (C) 2013  Aleksey Demakov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ARCH_X86_64_COMMON_H
+#define ARCH_X86_64_COMMON_H
+
+/* General purpose register width. */
+/*#define MM_WORD_32BIT		0*/
+#define MM_WORD_64BIT		1
+
+/* Cache line size. */
+#define MM_CACHELINE		(64)
+
+/* Virtual memory page size. */
+#define MM_PAGE_SIZE		(4096)
+
+#endif /* ARCH_X86_64_COMMON_H */
