1b97684e89c97a265b50e55eb20994d3d3c538ae
Aleksey Demakov <ademakov@gmail.com>
2013-05-18 02:57:11 +0400

    Add arch-specific atomic primitives for spin locks.

---
+++ src/arch/atomic.h
@@ -0,0 +1,33 @@
+/*
+ * arch/atomic.h - MainMemory arch-specific atomic ops.
+ *
+ * Copyright (C) 2013  Aleksey Demakov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ARCH_ATOMIC_H
+#define ARCH_ATOMIC_H
+
+#include "config.h"
+
+#if ARCH_X86
+# include "arch/x86/atomic.h"
+#elif ARCH_X86_64
+# include "arch/x86-64/atomic.h"
+#else
+# include "arch/generic/atomic.h"
+#endif
+
+#endif /* ARCH_ATOMIC_H */
---
+++ src/arch/generic/atomic.h
@@ -0,0 +1,59 @@
+/*
+ * arch/generic/atomic.h - MainMemory arch-specific atomic ops.
+ *
+ * Copyright (C) 2013  Aleksey Demakov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ARCH_GENERIC_ATOMIC_H
+#define ARCH_GENERIC_ATOMIC_H
+
+/**********************************************************************
+ * Atomic operations for spin-locks.
+ **********************************************************************/
+
+/*
+ * mm_atomic_lock_acquire() is a test-and-set atomic operation along with
+ * acquire fence.
+ * 
+ * mm_atomic_lock_release() is a simple clear operation along with release
+ * fence.
+ * 
+ * mm_atomic_lock_pause() is a special instruction to be used in spin-lock
+ * loops to make hyper-threading CPUs happy.
+ */
+
+#define MM_ATOMIC_LOCK_INIT	(0)
+
+typedef struct { char locked; } mm_atomic_lock_t;
+
+static inline int
+mm_atomic_lock_acquire(mm_atomic_lock_t *lock)
+{
+	return __sync_lock_test_and_set(&lock->locked, 1);
+}
+
+static inline void
+mm_atomic_lock_release(mm_atomic_lock_t *lock)
+{
+	__sync_lock_release(&lock->locked);
+}
+
+static inline void
+mm_atomic_lock_pause(void)
+{
+}
+
+#endif /* ARCH_GENERIC_ATOMIC_H */
