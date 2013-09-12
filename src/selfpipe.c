c062a20b31fdc4fe7e5b36fe7847e69692629b00
Aleksey Demakov <ademakov@gmail.com>
2013-09-12 21:20:52 +0400

    Comment selfpipe code.

--- src/selfpipe.c
+++ src/selfpipe.c
@@ -78,10 +78,33 @@ mm_selfpipe_notify(struct mm_selfpipe *selfpipe)
 {
 	ENTER();
 
+	// Take the notification into account. But it will be really
+	// delivered to the listening side only when it makes a next
+	// mm_selfpipe_listen() call.
 	mm_atomic_uint32_inc(&selfpipe->count);
 
 	mm_memory_fence();
 
+	// Make a write() call to the pipe if the listening side of the
+	// self-pipe is currently indeed listening. The objective here is
+	// to save expensive system calls when this does not contribute to
+	// the overall progress.
+	//
+	// Note that this logic is far from precise. For instance, this
+	// thread might get preempted on the write() call. And during this
+	// time the listening side might wakeup for a different reason,
+	// perform one or more full event-handling cycles and consume the
+	// notification held in the "count" field. And then this thread
+	// gets CPU time again and really finishes the write call causing
+	// spurious wakeup on the listening side.
+	//
+	// The key here is that no notification is ever lost or delayed
+	// rather than no extra write() call is ever made. Without exact
+	// control over the thread scheduling policy the only safe option
+	// is to make sure that the time span of a write() call always
+	// overlaps the time span of the corresponding listening side poll().
+	// Even if the error, as in the example above, might be as large as
+	// several other poll() cycles.
 	if (mm_memory_load(selfpipe->listen)) {
 		(void) write(selfpipe->write_fd, "", 1);
 	}
@@ -89,26 +112,39 @@ mm_selfpipe_notify(struct mm_selfpipe *selfpipe)
 	LEAVE();
 }
 
+/*
+ * Signal that the listening side of the self-pipe is really going into
+ * the listening mode and thus it will be needed to notify it thru the
+ * pipe write() calls. The return value indicates if there were any
+ * notifications while the listening side was not really listening.
+ */
 bool
 mm_selfpipe_listen(struct mm_selfpipe *selfpipe)
 {
 	ENTER();
 
+	// Drain the stalled notifications if any.
 	if (selfpipe->ready) {
 		mm_selfpipe_drain(selfpipe);
 		selfpipe->ready = false;
 	}
 
+	// Signal that the listening side is here.
 	mm_memory_store(selfpipe->listen, true);
 
 	mm_memory_fence();
 
-	uint32_t w = mm_atomic_uint32_fetch_and_set(&selfpipe->count, 0);
+	// See if there were some unseen notifications pending.
+	uint32_t n = mm_atomic_uint32_fetch_and_set(&selfpipe->count, 0);
 
 	LEAVE();
-	return (w != 0);
+	return (n != 0);
 }
 
+/*
+ * Signal that the listening side of the self-pipe is going to be busy
+ * with something else so there is no point to make pipe write() calls.
+ */
 void
 mm_selfpipe_divert(struct mm_selfpipe *selfpipe)
 {
