d3649b7dfdd06962633f844247c659b644f1f7c1
Aleksey Demakov <ademakov@gmail.com>
2013-09-24 19:44:58 +0400

    Update function name.

--- src/trace.c
+++ src/trace.c
@@ -48,7 +48,7 @@ mm_trace_prefix(void)
 	if (likely(mm_running_task != NULL)) {
 		mm_log_fmt("[%s][%d %s] %*s",
 			   mm_thread_name(),
-			   mm_task_id(mm_running_task),
+			   mm_task_getid(mm_running_task),
 			   mm_running_task->name,
 			   mm_running_task->trace_level * 2, "");
 	} else {
