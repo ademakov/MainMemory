e5cd2ece6e1b8bf8dde0bd56f9f9d5a6de1507c4
Aleksey Demakov <ademakov@gmail.com>
2014-04-01 18:06:20 +0700

    Nothing at all.

--- src/work.c
+++ src/work.c
@@ -140,7 +140,7 @@ mm_work_get(struct mm_workq *queue)
 
 	queue->queue_size--;
 	struct mm_link *link = mm_queue_delete_head(&queue->queue);
-	struct mm_work *work =  containerof(link, struct mm_work, link);
+	struct mm_work *work = containerof(link, struct mm_work, link);
 
 	LEAVE();
 	return work;
