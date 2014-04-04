f8967651629bd149e3859624658d881c5c228365
Aleksey Demakov <ademakov@gmail.com>
2014-04-04 20:28:29 +0700

    Update coopyright year.

--- src/hook.c
+++ src/hook.c
@@ -1,7 +1,7 @@
 /*
  * hook.c - MainMemory hook routines.
  *
- * Copyright (C) 2013  Aleksey Demakov
+ * Copyright (C) 2013-2014  Aleksey Demakov
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
