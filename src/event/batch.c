b9d15b294c404c297a6f44cab6c57acf683820f5
Aleksey Demakov <ademakov@gmail.com>
2015-02-08 14:48:49 +0600

    Add event batches.

---
+++ src/event/batch.c
@@ -0,0 +1,78 @@
+/*
+ * event/batch.c - MainMemory event batch.
+ *
+ * Copyright (C) 2015  Aleksey Demakov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "event/batch.h"
+
+#include "base/log/error.h"
+#include "base/log/trace.h"
+#include "base/mem/space.h"
+
+#define MM_EVENT_NEVENTS_MAX	(32 * MM_EVENT_NEVENTS)
+
+void __attribute__((nonnull(1)))
+mm_event_batch_prepare(struct mm_event_batch *batch)
+{
+	ENTER();
+
+	batch->flags = 0;
+	batch->nevents = 0;
+	batch->nevents_max = MM_EVENT_NEVENTS;
+	batch->events = mm_common_alloc(batch->nevents_max * sizeof(struct mm_event));
+
+	LEAVE();
+}
+
+void __attribute__((nonnull(1)))
+mm_event_batch_cleanup(struct mm_event_batch *batch)
+{
+	ENTER();
+
+	mm_common_free(batch->events);
+
+	LEAVE();
+}
+
+void __attribute__((nonnull(1)))
+mm_event_batch_expand(struct mm_event_batch *batch)
+{
+	ENTER();
+
+	if (unlikely(batch->nevents_max == MM_EVENT_NEVENTS_MAX))
+		mm_fatal(0, "too many events");
+
+	batch->nevents_max *= 2;
+	batch->events = mm_common_realloc(batch->events, batch->nevents_max * sizeof(struct mm_event));
+
+	LEAVE();
+}
+
+void __attribute__((nonnull(1, 2)))
+mm_event_batch_append(struct mm_event_batch *batch, struct mm_event_batch *batch2)
+{
+	ENTER();
+
+	mm_event_batch_setflags(batch, batch2->flags);
+
+	for (int i = 0; i < batch2->nevents; i++) {
+		struct mm_event *event = &batch2->events[i];
+		mm_event_batch_add(batch, event->event, event->ev_fd);
+	}
+
+	LEAVE();
+}
---
+++ src/event/batch.h
@@ -0,0 +1,79 @@
+/*
+ * event/batch.h - MainMemory event batch.
+ *
+ * Copyright (C) 2015  Aleksey Demakov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef EVENT_BATCH_H
+#define EVENT_BATCH_H
+
+#include "common.h"
+#include "event/event.h"
+
+#define MM_EVENT_BATCH_REGISTER		(1)
+#define MM_EVENT_BATCH_UNREGISTER	(2)
+
+struct mm_event_batch
+{
+	int flags;
+	int nevents;
+	int nevents_max;
+	struct mm_event *events;
+};
+
+void __attribute__((nonnull(1)))
+mm_event_batch_prepare(struct mm_event_batch *batch);
+
+void __attribute__((nonnull(1)))
+mm_event_batch_cleanup(struct mm_event_batch *batch);
+
+void __attribute__((nonnull(1)))
+mm_event_batch_expand(struct mm_event_batch *batch);
+
+void __attribute__((nonnull(1, 2)))
+mm_event_batch_append(struct mm_event_batch *batch, struct mm_event_batch *batch2);
+
+static inline void __attribute__((nonnull(1)))
+mm_event_batch_setflags(struct mm_event_batch *batch, int flags)
+{
+	batch->nevents |= flags;
+}
+
+static inline bool __attribute__((nonnull(1)))
+mm_event_batch_hasflags(struct mm_event_batch *batch, int flags)
+{
+	return (batch->nevents | flags) != 0;
+}
+
+static inline void __attribute__((nonnull(1)))
+mm_event_batch_add(struct mm_event_batch *batch, mm_event_t event, struct mm_event_fd *ev_fd)
+{
+	if (unlikely(batch->nevents == batch->nevents_max))
+		mm_event_batch_expand(batch);
+
+	batch->events[batch->nevents].event = event;
+	batch->events[batch->nevents].ev_fd = ev_fd;
+	batch->nevents++;
+}
+
+static inline void __attribute__((nonnull(1)))
+mm_event_batch_clear(struct mm_event_batch *batch)
+{
+	batch->flags = 0;
+	batch->nevents = 0;
+}
+
+#endif /* EVENT_BATCH_H */
