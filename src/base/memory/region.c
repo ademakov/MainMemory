35a7fec651b392def68fbc5653446c079fd6051c
Aleksey Demakov <ademakov@gmail.com>
2015-09-27 23:53:15 +0600

    Add a minimal region allocator

---
+++ src/base/memory/region.c
@@ -0,0 +1,96 @@
+/*
+ * base/memory/region.c - MainMemory region allocator.
+ *
+ * Copyright (C) 2015  Aleksey Demakov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "base/memory/region.h"
+
+#include "base/log/error.h"
+#include "base/log/trace.h"
+#include "base/memory/memory.h"
+
+void __attribute__((nonnull(1)))
+mm_region_prepare(struct mm_region *reg)
+{
+	ENTER();
+
+	reg->block_ptr = NULL;
+	reg->block_end = NULL;
+	reg->chunk_end = NULL;
+
+	mm_stack_prepare(&reg->chunks);
+
+	LEAVE();
+}
+
+void __attribute__((nonnull(1)))
+mm_region_cleanup(struct mm_region *reg)
+{
+	ENTER();
+
+	mm_chunk_destroy_stack(&reg->chunks);
+
+	LEAVE();
+}
+
+void __attribute__((nonnull(1)))
+mm_region_reserve(struct mm_region *reg, size_t more_size)
+{
+	ENTER();
+
+	// Find out the required memory block size.
+	size_t old_size = reg->block_end - reg->block_ptr;
+	size_t new_size = old_size + more_size;
+
+	// Find out the required memory chunk size. It has to fit the required
+	// block plus a tiny bit for the initial block alignment, and provide
+	// some extra room to amortize further allocation.
+	size_t chunk_size = MM_REGION_CHUNK_SIZE;
+	if (new_size > (3 * MM_REGION_CHUNK_SIZE / 4))
+		chunk_size = new_size + new_size / 2;
+	if (unlikely(chunk_size < more_size) || unlikely(chunk_size < old_size))
+		mm_fatal(EOVERFLOW, "chunk size overflow");
+
+	// Create a new memory chunk.
+	struct mm_chunk *chunk = mm_chunk_create(new_size);
+	reg->chunk_end = chunk->data + mm_chunk_getsize(chunk);
+	mm_stack_insert(&reg->chunks, &chunk->base.slink);
+
+	// Align the initial block address.
+	uintptr_t addr = (uintptr_t) chunk->data;
+	addr = mm_round_up(addr, MM_REGION_ALIGN);
+	char *ptr = (char *) addr;
+
+	// Copy the old block content if any.
+	if (old_size) {
+		memcpy(ptr, reg->block_ptr, old_size);
+
+		// Free the old chunk if it was entirely used for the old block.
+		struct mm_chunk *old_chunk = mm_chunk_from_slink(chunk->base.slink.next);
+		addr = (uintptr_t) old_chunk->data;
+		addr = mm_round_up(addr, MM_REGION_ALIGN);
+		if (reg->block_ptr == (char *) addr) {
+			mm_stack_remove_next(&chunk->base.slink);
+			mm_chunk_destroy(old_chunk);
+		}
+	}
+
+	reg->block_ptr = ptr;
+	reg->block_end = ptr + old_size;
+
+	LEAVE();
+}
---
+++ src/base/memory/region.h
@@ -0,0 +1,149 @@
+/*
+ * base/memory/region.h - MainMemory region allocator.
+ *
+ * Copyright (C) 2015  Aleksey Demakov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BASE_MEMORY_REGION_H
+#define BASE_MEMORY_REGION_H
+
+#include "common.h"
+#include "base/bitops.h"
+#include "base/log/debug.h"
+#include "base/memory/chunk.h"
+
+#define MM_REGION_ALIGN		(sizeof(uintptr_t))
+#define MM_REGION_CHUNK_SIZE	(4 * 1024 - MM_CHUNK_OVERHEAD)
+
+struct mm_region
+{
+	/* The last allocated memory block. */
+	char *block_ptr;
+	/* The last allocated memory block's end. */
+	char *block_end;
+	/* The currently used memory chunk's end. */
+	char *chunk_end;
+	/* Entire region memory as a list of chunks. */
+	struct mm_stack chunks;
+};
+
+void __attribute__((nonnull(1)))
+mm_region_prepare(struct mm_region *reg);
+
+void __attribute__((nonnull(1)))
+mm_region_cleanup(struct mm_region *reg);
+
+void __attribute__((nonnull(1)))
+mm_region_reserve(struct mm_region *reg, size_t more_size);
+
+static inline size_t
+mm_region_round_size(size_t size)
+{
+	return mm_round_up(size, MM_REGION_ALIGN);
+}
+
+static inline bool __attribute__((nonnull(1)))
+mm_region_empty(struct mm_region *reg)
+{
+	return (reg->block_ptr == NULL);
+}
+
+static inline bool __attribute__((nonnull(1)))
+mm_region_whole(struct mm_region *reg)
+{
+	return (reg->block_ptr == reg->block_end);
+}
+
+static inline size_t __attribute__((nonnull(1)))
+mm_region_getroom(struct mm_region *reg)
+{
+	return (reg->chunk_end - reg->block_end);
+}
+
+static inline size_t __attribute__((nonnull(1)))
+mm_region_getblocksize(struct mm_region *reg)
+{
+	return (reg->block_end - reg->block_ptr);
+}
+
+static inline void * __attribute__((nonnull(1)))
+mm_region_getblockbase(struct mm_region *reg)
+{
+	return reg->block_ptr;
+}
+
+/*
+ * Incrementally allocate a memory block without checking if there is enough
+ * memory room for it. Returns a pointer to the additional memory.
+ */
+static inline void * __attribute__((nonnull(1)))
+mm_region_extend_fast(struct mm_region *reg, size_t size)
+{
+	char *end = reg->block_end;
+	reg->block_end += size;
+	return end;
+}
+
+/*
+ * Incrementally allocate a memory block and make ensure the current memory
+ * chunk is large enough to accommodate the requested block size increment.
+ * Returns a pointer to the additional memory.
+ */
+static inline void * __attribute__((nonnull(1)))
+mm_region_extend(struct mm_region *reg, size_t size)
+{
+	if (mm_region_getroom(reg) < size)
+		mm_region_reserve(reg, size);
+	return mm_region_extend_fast(reg, size);
+}
+
+/* Finalize an incrementally allocated block. */
+static inline void * __attribute__((nonnull(1)))
+mm_region_finish(struct mm_region *reg)
+{
+	/* For a block with zero size force a minimal allocation. */
+	if (unlikely(reg->block_ptr == reg->block_end)) {
+		if (unlikely(reg->block_end == reg->chunk_end))
+			mm_region_reserve(reg, 1);
+		reg->block_end++;
+	}
+
+	/* Ensure a proper alignment of the next allocated block
+	   rounding the size of the current one. */
+	uintptr_t addr = (uintptr_t) reg->block_end;
+	addr = mm_round_up(addr, MM_REGION_ALIGN);
+	reg->block_end = (char *) addr;
+
+	/* But beware of running over the chunk boundary. */
+	if (unlikely(reg->block_end > reg->chunk_end))
+		reg->block_end = reg->chunk_end;
+
+	/* Finally move past the block and return a pointer to its start. */
+	char *ptr = reg->block_ptr;
+	reg->block_ptr = reg->block_end;
+	return ptr;
+}
+
+/* Allocate at once a whole memory block. */
+static inline void * __attribute__((nonnull(1)))
+mm_region_alloc(struct mm_region *reg, size_t size)
+{
+	ASSERT(mm_region_whole(reg));
+	mm_region_extend(reg, size);
+	return mm_region_finish(reg);
+}
+
+#endif /* BASE_MEMORY_REGION_H */
