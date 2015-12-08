9759d9b62d532cde0910b5e850b5506bf7d60b3a
Aleksey Demakov <ademakov@gmail.com>
2015-12-09 00:22:31 +0600

    Optimize syscalls on darwin

--- src/arch/x86-64/syscall.h
+++ src/arch/x86-64/syscall.h
@@ -20,19 +20,24 @@
 #ifndef ARCH_X86_64_SYSCALL_H
 #define ARCH_X86_64_SYSCALL_H
 
+#include "common.h"
+
 #ifdef __APPLE__
 
 # define MM_SYSCALL_N(n)	(n | (2 << 24))
 
-static inline intptr_t
-mm_syscall_result(uintptr_t result, uint8_t error)
-{
-	if (error) {
-		errno = result;
-		result = -1;
-	}
-	return (intptr_t) result;
-}
+#if __GCC_ASM_FLAG_OUTPUTS__
+# define MM_SYSCALL_INST	"syscall"
+# define MM_SYSCALL_OUTPUTS	"=a"(result), "=@ccc"(error)
+# define MM_SYSCALL_CLOBBER	"memory", "r11"
+#else
+# define MM_SYSCALL_INST	"syscall; setc %1"
+# define MM_SYSCALL_OUTPUTS	"=a"(result), "=c"(error)
+# define MM_SYSCALL_CLOBBER	"cc", "memory", "r11"
+#endif
+
+intptr_t
+mm_syscall_error(uintptr_t result);
 
 static inline intptr_t
 mm_syscall_0(int n)
@@ -40,12 +45,11 @@ mm_syscall_0(int n)
 	uintptr_t result;
 	uint8_t error;
 
-	__asm__ __volatile__("syscall\n\t"
-			     "setc %1"
-			     : "=a"(result), "=c"(error)
+	__asm__ __volatile__(MM_SYSCALL_INST
+			     : MM_SYSCALL_OUTPUTS
 			     : "0"(n)
-			     : "cc", "memory", "r11");
-	return mm_syscall_result(result, error);
+			     : MM_SYSCALL_CLOBBER);
+	return error ? mm_syscall_error(result) : (intptr_t) result;
 }
 
 static inline intptr_t
@@ -54,12 +58,11 @@ mm_syscall_1(int n, uintptr_t a1)
 	uintptr_t result;
 	uint8_t error;
 
-	__asm__ __volatile__("syscall\n\t"
-			     "setc %1"
-			     : "=a"(result), "=c"(error)
+	__asm__ __volatile__(MM_SYSCALL_INST
+			     : MM_SYSCALL_OUTPUTS
 			     : "0"(n), "D"(a1)
-			     : "cc", "memory", "r11");
-	return mm_syscall_result(result, error);
+			     : MM_SYSCALL_CLOBBER);
+	return error ? mm_syscall_error(result) : (intptr_t) result;
 }
 
 static inline intptr_t
@@ -68,12 +71,11 @@ mm_syscall_2(int n, uintptr_t a1, uintptr_t a2)
 	uintptr_t result;
 	uint8_t error;
 
-	__asm__ __volatile__("syscall\n\t"
-			     "setc %1"
-			     : "=a"(result), "=c"(error)
+	__asm__ __volatile__(MM_SYSCALL_INST
+			     : MM_SYSCALL_OUTPUTS
 			     : "0"(n), "D"(a1), "S"(a2)
-			     : "cc", "memory", "r11");
-	return mm_syscall_result(result, error);
+			     : MM_SYSCALL_CLOBBER);
+	return error ? mm_syscall_error(result) : (intptr_t) result;
 }
 
 static inline intptr_t
@@ -82,12 +84,11 @@ mm_syscall_3(int n, uintptr_t a1, uintptr_t a2, uintptr_t a3)
 	uintptr_t result;
 	uint8_t error;
 
-	__asm__ __volatile__("syscall\n\t"
-			     "setc %1"
-			     : "=a"(result), "=c"(error)
+	__asm__ __volatile__(MM_SYSCALL_INST
+			     : MM_SYSCALL_OUTPUTS
 			     : "0"(n), "D"(a1), "S"(a2), "d"(a3)
-			     : "cc", "memory", "r11");
-	return mm_syscall_result(result, error);
+			     : MM_SYSCALL_CLOBBER);
+	return error ? mm_syscall_error(result) : (intptr_t) result;
 }
 
 static inline intptr_t
@@ -97,12 +98,11 @@ mm_syscall_4(int n, uintptr_t a1, uintptr_t a2, uintptr_t a3, uintptr_t a4)
 	uint8_t error;
 
 	register uintptr_t r4 __asm__("r10") = a4;
-	__asm__ __volatile__("syscall\n\t"
-			     "setc %1"
-			     : "=a"(result), "=c"(error)
+	__asm__ __volatile__(MM_SYSCALL_INST
+			     : MM_SYSCALL_OUTPUTS
 			     : "0"(n), "D"(a1), "S"(a2), "d"(a3), "r"(r4)
-			     : "cc", "memory", "r11");
-	return mm_syscall_result(result, error);
+			     : MM_SYSCALL_CLOBBER);
+	return error ? mm_syscall_error(result) : (intptr_t) result;
 }
 
 static inline intptr_t
@@ -114,12 +114,11 @@ mm_syscall_5(int n, uintptr_t a1, uintptr_t a2, uintptr_t a3, uintptr_t a4,
 
 	register uintptr_t r4 __asm__("r10") = a4;
 	register uintptr_t r5 __asm__("r8") = a5;
-	__asm__ __volatile__("syscall\n\t"
-			     "setc %1"
-			     : "=a"(result), "=c"(error)
+	__asm__ __volatile__(MM_SYSCALL_INST
+			     : MM_SYSCALL_OUTPUTS
 			     : "0"(n), "D"(a1), "S"(a2), "d"(a3), "r"(r4), "r"(r5)
-			     : "cc", "memory", "r11");
-	return mm_syscall_result(result, error);
+			     : MM_SYSCALL_CLOBBER);
+	return error ? mm_syscall_error(result) : (intptr_t) result;
 }
 
 static inline intptr_t
@@ -132,12 +131,11 @@ mm_syscall_6(int n, uintptr_t a1, uintptr_t a2, uintptr_t a3, intptr_t a4,
 	register uintptr_t r4 __asm__("r10") = a4;
 	register uintptr_t r5 __asm__("r8") = a5;
 	register uintptr_t r6 __asm__("r9") = a6;
-	__asm__ __volatile__("syscall\n\t"
-			     "setc %1"
-			     : "=a"(result), "=c"(error)
+	__asm__ __volatile__(MM_SYSCALL_INST
+			     : MM_SYSCALL_OUTPUTS
 			     : "0"(n), "D"(a1), "S"(a2), "d"(a3), "r"(r4), "r"(r5), "r"(r6)
-			     : "cc", "memory", "r11");
-	return mm_syscall_result(result, error);
+			     : MM_SYSCALL_CLOBBER);
+	return error ? mm_syscall_error(result) : (intptr_t) result;
 }
 
 #else /* !__APPLE__ */
