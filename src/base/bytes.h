ed6d3f5d83b46fb0c0884db60b8802494f765009
Aleksey Demakov <ademakov@gmail.com>
2019-06-13 01:18:50 +0300

    Use const pointer for byte ops

--- src/base/bytes.h
+++ src/base/bytes.h
@@ -97,74 +97,74 @@ mm_bswap64(uint64_t x)
  **********************************************************************/
 
 static inline uint16_t
-mm_load_be16(void *p)
+mm_load_be16(const void *p)
 {
 #if ARCH_X86 || ARCH_X86_64
-	uint16_t *v = p;
+	const uint16_t *v = p;
 	return mm_betoh16(*v);
 #else
-	uint8_t *b = p;
+	const uint8_t *b = p;
 	return ((uint16_t) b[0] << 8) | (uint16_t) b[1];
 #endif
 }
 
 static inline uint32_t
-mm_load_be32(void *p)
+mm_load_be32(const void *p)
 {
 #if ARCH_X86 || ARCH_X86_64
-	uint32_t *v = p;
+	const uint32_t *v = p;
 	return mm_betoh32(*v);
 #else
-	uint8_t *b = p;
+	const uint8_t *b = p;
 	return ((uint32_t) b[0] << 24) | ((uint32_t) b[1] << 16) | ((uint32_t) b[2] << 8) | (uint32_t) b[3];
 #endif
 }
 
 static inline uint64_t
-mm_load_be64(void *p)
+mm_load_be64(const void *p)
 {
 #if ARCH_X86 || ARCH_X86_64
-	uint64_t *v = p;
+	const uint64_t *v = p;
 	return mm_betoh64(*v);
 #else
-	uint8_t *b = p;
+	const uint8_t *b = p;
 	return (((uint64_t) b[0] << 56) | ((uint64_t) b[1] << 48) | ((uint64_t) b[2] << 40) | ((uint64_t) b[3] << 32)
 		| ((uint64_t) b[4] << 24) | ((uint64_t) b[5] << 16) | ((uint64_t) b[6] << 8) | (uint64_t) b[7]);
 #endif
 }
 
 static inline uint16_t
-mm_load_le16(void *p)
+mm_load_le16(const void *p)
 {
 #if ARCH_X86 || ARCH_X86_64
-	uint16_t *v = p;
+	const uint16_t *v = p;
 	return *v;
 #else
-	uint8_t *b = p;
+	const uint8_t *b = p;
 	return (uint16_t) b[0] | ((uint16_t) b[1] << 8);
 #endif
 }
 
 static inline uint32_t
-mm_load_le32(void *p)
+mm_load_le32(const void *p)
 {
 #if ARCH_X86 || ARCH_X86_64
-	uint32_t *v = p;
+	const uint32_t *v = p;
 	return *v;
 #else
-	uint8_t *b = p;
+	const uint8_t *b = p;
 	return (uint32_t) b[0] | ((uint32_t) b[1] << 8) | ((uint32_t) b[2] << 16) | ((uint32_t) b[3] << 24);
 #endif
 }
 
 static inline uint64_t
-mm_load_le64(void *p)
+mm_load_le64(const void *p)
 {
 #if ARCH_X86 || ARCH_X86_64
-	uint64_t *v = p;
+	const uint64_t *v = p;
 	return *v;
 #else
-	uint8_t *b = p;
+	const uint8_t *b = p;
 	return ((uint64_t) b[0] | ((uint64_t) b[1] << 8) | ((uint64_t) b[2] << 16) | ((uint64_t) b[3] << 24)
 		| ((uint64_t) b[4] << 32) | ((uint64_t) b[5] << 40) | ((uint64_t) b[6] << 48) | ((uint64_t) b[7] << 56));
 #endif
