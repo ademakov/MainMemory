100d27f689f9664004774bd171a6083a2974a2c1
Aleksey Demakov <ademakov@gmail.com>
2019-11-17 13:06:54 +0300

    Clean up the cstack header for x86

--- src/base/arch/x86/cstack.h
+++ src/base/arch/x86/cstack.h
@@ -1,7 +1,7 @@
 /*
- * base/cstack.h - MainMemory arch-specific call stack support.
+ * base/arch/x86/cstack.h - MainMemory arch-specific call stack support.
  *
- * Copyright (C) 2012-2014  Aleksey Demakov
+ * Copyright (C) 2012-2019  Aleksey Demakov
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,37 +17,12 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef BASE_CSTACK_H
-#define BASE_CSTACK_H
+#ifndef BASE_ARCH_X86_CSTACK_H
+#define BASE_ARCH_X86_CSTACK_H
 
-#include "common.h"
-
-#include <stddef.h>
-
-#if ARCH_GENERIC
-# if HAVE_UCONTEXT_H
-#  include <ucontext.h>
-# else
-#  error "Unsupported architecture."
-# endif
-#endif
-
-#if ARCH_GENERIC
-typedef ucontext_t mm_cstack_t;
-#else
 typedef void * mm_cstack_t;
-#endif
-
-void *
-mm_cstack_create(uint32_t stack_size, uint32_t guard_size);
-
-void NONNULL(1)
-mm_cstack_destroy(void *stack, uint32_t stack_size);
-
-void NONNULL(1, 2)
-mm_cstack_prepare(mm_cstack_t *ctx, void (*entry)(void), char *stack, size_t size);
 
 void NONNULL(1, 2)
 mm_cstack_switch(mm_cstack_t *old_ctx, mm_cstack_t *new_ctx);
 
-#endif /* BASE_CSTACK_H */
+#endif /* BASE_ARCH_X86_CSTACK_H */
