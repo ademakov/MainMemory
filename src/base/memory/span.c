7d21fb48098cb35ba0c2f70e8cf2173144f36dfb
Aleksey Demakov <ademakov@gmail.com>
2020-02-14 00:30:43 +0300

    More memory span code

--- src/base/memory/span.c
+++ src/base/memory/span.c
@@ -19,12 +19,14 @@
 
 #include "base/memory/span.h"
 
+#include "base/bitops.h"
 #include "base/report.h"
+#include "base/memory/cache.h"
 
 #include <sys/mman.h>
 
 static void
-mm_memory_free_space(void *addr, size_t size)
+mm_memory_free_space(void *const addr, const size_t size)
 {
 	if (unlikely(munmap(addr, size) < 0))
 		mm_fatal(errno, "munmap()");
@@ -33,8 +35,7 @@ mm_memory_free_space(void *addr, size_t size)
 static void *
 mm_memory_alloc_space(const size_t size, const size_t addr_mask)
 {
-	ASSERT(size != 0);
-	ASSERT((size & (MM_PAGE_SIZE - 1)) == 0);
+	ASSERT(mm_is_pow2(size));
 
 	// Allocate a span speculatively assuming that it will be aligned as required.
 	void *addr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
@@ -46,8 +47,11 @@ mm_memory_alloc_space(const size_t size, const size_t addr_mask)
 		mm_memory_free_space(addr, size);
 
 		const size_t upsized_size = size + addr_mask - MM_PAGE_SIZE + 1;
-		if (upsized_size < size) // check for integer aritmetic overflow
+		if (upsized_size < size) {
+			// integer aritmetic overflow
+			errno = EOVERFLOW;
 			return NULL;
+		}
 
 		void *upsized_addr = mmap(NULL, upsized_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 		if (upsized_addr == MAP_FAILED)
@@ -65,51 +69,95 @@ mm_memory_alloc_space(const size_t size, const size_t addr_mask)
 	return addr;
 }
 
-struct mm_memory_span *
-mm_memory_span_create_heap(struct mm_context *context, struct mm_memory_cache *cache)
+struct mm_memory_span * NONNULL(1)
+mm_memory_span_create_heap(struct mm_memory_cache *const cache, const bool active)
 {
 	ENTER();
 
-	struct mm_memory_span *span = mm_memory_alloc_space(MM_MEMORY_SPAN_HEAP_SIZE,
-							    MM_MEMORY_SPAN_ALIGNMENT_MASK);
-	span->type_and_size = 1;
-	span->resident_size = MM_MEMORY_SPAN_HEAP_SIZE;
-	span->context = context;
-	span->cache = cache;
-	// TODO: cache_link
+	struct mm_memory_span *span = mm_memory_alloc_space(MM_MEMORY_SPAN_HEAP_SIZE, MM_MEMORY_SPAN_ALIGNMENT_MASK);
+	if (likely(span != NULL)) {
+		span->type_and_size = active ? MM_MEMORY_SPAN_ACTIVE_HEAP : MM_MEMORY_SPAN_STAGING_HEAP;
+		span->resident_size = MM_MEMORY_SPAN_HEAP_SIZE;
+
+		span->cache = cache;
+		span->context = cache->context;
+
+		if (active) {
+			VERIFY(cache->active == NULL);
+			cache->active = span;
+		} else {
+			mm_list_insert(&cache->heap, &span->cache_link);
+		}
+	}
 
 	LEAVE();
 	return span;
 }
 
-struct mm_memory_span *
-mm_memory_span_create_huge(struct mm_context *context, struct mm_memory_cache *cache, size_t size)
+struct mm_memory_span * NONNULL(1)
+mm_memory_span_create_huge(struct mm_memory_cache *const cache, const size_t size)
 {
 	ENTER();
 
-	size_t span_size = sizeof(struct mm_memory_span) + size;
-	span_size = (size + MM_PAGE_SIZE) & ~MM_PAGE_SIZE;
-	if (unlikely(span_size < size)) // check for integer aritmetic overflow
-		return NULL;
+	struct mm_memory_span *span = NULL;
 
-	struct mm_memory_span *span = mm_memory_alloc_space(span_size,
-							    MM_MEMORY_SPAN_ALIGNMENT_MASK);
-	span->type_and_size = size;
-	span->resident_size = size;
-	span->context = context;
-	span->cache = cache;
-	// TODO: cache_link
+	const size_t span_size = mm_round_up(sizeof(struct mm_memory_span) + size, MM_PAGE_SIZE);
+	if (unlikely(span_size < size)) {
+		// integer aritmetic overflow
+		errno = EOVERFLOW;
+		goto leave;
+	}
 
+	span = mm_memory_alloc_space(span_size, MM_MEMORY_SPAN_ALIGNMENT_MASK);
+	if (likely(span != NULL)) {
+		span->type_and_size = span_size;
+		span->resident_size = span_size;
+
+		span->cache = cache;
+		span->context = cache->context;
+
+		mm_list_insert(&cache->huge, &span->cache_link);
+	}
+
+leave:
 	LEAVE();
 	return span;
 }
 
-void
-mm_memory_span_destroy(struct mm_memory_span *span)
+void NONNULL(1)
+mm_memory_span_activate(struct mm_memory_span *const span)
+{
+	ENTER();
+	ASSERT(mm_memory_span_staging(span));
+	ASSERT(span->cache->active == NULL);
+
+	span->type_and_size = MM_MEMORY_SPAN_ACTIVE_HEAP;
+	mm_list_delete(&span->cache_link);
+	span->cache->active = span;
+
+	LEAVE();
+}
+
+void NONNULL(1)
+mm_memory_span_deactivate(struct mm_memory_span *const span)
+{
+	ENTER();
+	ASSERT(mm_memory_span_active(span));
+	ASSERT(span->cache->active == span);
+
+	span->type_and_size = MM_MEMORY_SPAN_STAGING_HEAP;
+	mm_list_insert(&span->cache->heap, &span->cache_link);
+	span->cache->active = NULL;
+
+	LEAVE();
+}
+
+void NONNULL(1)
+mm_memory_span_destroy(struct mm_memory_span *const span)
 {
 	ENTER();
 
-	mm_memory_free_space(span, mm_memory_span_size(span));
+	mm_memory_free_space(span, mm_memory_span_virtual_size(span));
 
 	LEAVE();
 }
--- src/base/memory/span.h
+++ src/base/memory/span.h
@@ -36,6 +36,11 @@
 /* Size of a regular span used as a storage for smaller objects. */
 #define MM_MEMORY_SPAN_HEAP_SIZE	MM_MEMORY_SPAN_ALIGNMENT
 
+/* Regular span types. */
+#define MM_MEMORY_SPAN_ACTIVE_HEAP	(0u)
+#define MM_MEMORY_SPAN_STAGING_HEAP	(1u)
+#define MM_MEMORY_SPAN_LAST_HEAP_TYPE	MM_MEMORY_SPAN_STAGING_HEAP
+
 /* Span descriptor. */
 struct mm_memory_span
 {
@@ -52,37 +57,65 @@ struct mm_memory_span
 };
 
 /* Get span descriptor for an address within 2MiB from its start. */
-static inline struct mm_memory_span *
+static inline struct mm_memory_span * NONNULL(1)
 mm_memory_span_from_ptr(void *ptr)
 {
 	return (struct mm_memory_span *) ((uintptr_t) ptr & ~MM_MEMORY_SPAN_ALIGNMENT_MASK);
 }
 
-static inline bool
-mm_memory_span_heap(struct mm_memory_span *span)
-{
-	return (span->type_and_size == 1);
-}
-
-static inline bool
+/* Check to see if the span is for a single huge chunk. */
+static inline bool NONNULL(1)
 mm_memory_span_huge(struct mm_memory_span *span)
 {
-	return (span->type_and_size != 1);
+	return (span->type_and_size > MM_MEMORY_SPAN_LAST_HEAP_TYPE);
 }
 
-static inline size_t
-mm_memory_span_size(struct mm_memory_span *span)
+/* Check to see if the span is for regular chunk allocation. */
+static inline bool NONNULL(1)
+mm_memory_span_heap(struct mm_memory_span *span)
+{
+	return (span->type_and_size <= MM_MEMORY_SPAN_LAST_HEAP_TYPE);
+}
+
+/* Check to see if the span is for regular chunk allocation and is currently active. */
+static inline bool NONNULL(1)
+mm_memory_span_active(struct mm_memory_span *span)
+{
+	return (span->type_and_size == MM_MEMORY_SPAN_ACTIVE_HEAP);
+}
+
+/* Check to see if the span is for regular chunk allocation but is currently on hold. */
+static inline bool NONNULL(1)
+mm_memory_span_staging(struct mm_memory_span *span)
+{
+	return (span->type_and_size == MM_MEMORY_SPAN_STAGING_HEAP);
+}
+
+static inline size_t NONNULL(1)
+mm_memory_span_virtual_size(struct mm_memory_span *span)
 {
 	return mm_memory_span_heap(span) ? MM_MEMORY_SPAN_HEAP_SIZE : span->type_and_size;
 }
 
-struct mm_memory_span *
-mm_memory_span_create_heap(struct mm_context *context, struct mm_memory_cache *cache);
+static inline size_t NONNULL(1)
+mm_memory_span_resident_size(struct mm_memory_span *span)
+{
+	return mm_memory_span_heap(span) ? MM_MEMORY_SPAN_HEAP_SIZE : span->resident_size;
+}
 
-struct mm_memory_span *
-mm_memory_span_create_huge(struct mm_context *context, struct mm_memory_cache *cache, size_t size);
+struct mm_memory_span * NONNULL(1)
+mm_memory_span_create_heap(struct mm_memory_cache *cache, bool active);
 
-void
+struct mm_memory_span * NONNULL(1)
+mm_memory_span_create_huge(struct mm_memory_cache *cache, size_t size);
+
+void NONNULL(1)
+mm_memory_span_activate(struct mm_memory_span *span);
+
+void NONNULL(1)
+mm_memory_span_deactivate(struct mm_memory_span *span);
+
+void NONNULL(1)
 mm_memory_span_destroy(struct mm_memory_span *span);
 
 #endif /* BASE_MEMORY_SPAN_H */
